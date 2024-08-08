9a3c6176ec81c7429bc87c7aae1d525f49140113
nelg62 <glen.harding.nz@gmail.com>
2024-08-08 14:40:21 +1200

    change backend cors file

--- miniproject2express/index.js
+++ miniproject2express/index.js
@@ -2,38 +2,32 @@ const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./routes/userDataRoutes");
 const swaggerUi = require("swagger-ui-express");
-swaggerDocument = require("./swagger.json");
+const swaggerDocument = require("./swagger.json");
 const app = express();
 const port = 3083;
 
 // Setup Swagger documentation at /api-docs
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// Added a limit to allow the express server side to recieve files as the images were being blocked for being to large
+// Added a limit to allow the express server side to receive files as the images were being blocked for being too large
 app.use(express.json({ limit: "10mb" }));
-// Allow cors on the nextjs ports to allow request to the server from frountend
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type"],
-//   })
-// );
+
+// Allow CORS for requests coming from Netlify
 app.use(
   cors({
     origin:
-      "https://66b42a2f65f0a100084b9e3a--miniproject2glenharding.netlify.app/",
+      "https://66b42a2f65f0a100084b9e3a--miniproject2glenharding.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
 );
+
 app.use("/", express.static("public"));
 
 // Use /users for the userDataRoutes paths
 app.use("/users", userRoutes);
 
-// Start server and listen on spesific port
+// Start server and listen on specific port
 app.listen(port, () => {
-  console.log(`Example app listening
-at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
